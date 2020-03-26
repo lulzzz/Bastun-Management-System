0f91c9fb504537d382e934ebd6ec1814bec97476
Mitko <34096775+Shemetnia@users.noreply.github.com>
2020-03-26 11:27:43 +0200

    Change MVT input models

---
+++ WebApplication1/GlobalData/ErrorMessages/InvalidDepartureMovementErrorMessages.cs
@@ -0,0 +1,13 @@
+﻿namespace BMS.GlobalData.ErrorMessages
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    public class InvalidDepartureMovementErrorMessages
+    {
+        public const string DepartureMovementIsRequired = "Departure movement is required!";
+
+        public const string DepartureMovementIsInvalid = "Departure movement format is invalid!";
+    }
+}
--- WebApplication1/Models/MovementsInputModels/ArrivalMovementInputModel.cs
+++ WebApplication1/Models/MovementsInputModels/ArrivalMovementInputModel.cs
@@ -2,19 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using BMS.GlobalData.ErrorMessages;
+using BMS.GlobalData.Validation;
 
 namespace WebApplication1.Models.MovementsInputModels
 {
     public class ArrivalMovementInputModel
     {
-        public DateTime TouchdownTime { get; set; }
-
-        public DateTime OnBlockTime { get; set; }
-
-        public string SupplementaryInformation { get; set; }
-
-        public DateTime DateOfMovement { get; set; }
-
-        public string FlightNumber { get; set; }
+        [Required(ErrorMessage =InvalidDepartureMovementErrorMessages.DepartureMovementIsRequired)]
+        [RegularExpression(MessagesValidation.DepMVTMessageValidation,ErrorMessage = InvalidDepartureMovementErrorMessages.DepartureMovementIsInvalid)]
+        public string Message { get; set; }
     }
 }
