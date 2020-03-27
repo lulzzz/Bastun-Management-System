8da5a4ce50e5a8860430f7bceb5fca2a0e76dcb6
Mitko <34096775+Shemetnia@users.noreply.github.com>
2020-03-27 08:53:40 +0200

    Implement PAX weight enum

---
+++ WebApplication1/Data/Models/Enums/PAXWeight.cs
@@ -0,0 +1,15 @@
+﻿namespace BMS.Data.Models.Enums
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    public enum PAXWeight
+    {
+        M = 88,
+        F = 70,
+        C = 35,
+        i = 0,
+    }
+}
