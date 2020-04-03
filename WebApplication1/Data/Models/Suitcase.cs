eb1c52330cdd0fc33f8d5f8de175f9166016fe2f
Mitko <34096775+Shemetnia@users.noreply.github.com>
2020-04-03 13:14:31 +0300

    Create pax input data validation class

--- WebApplication1/Data/Models/Suitcase.cs
+++ WebApplication1/Data/Models/Suitcase.cs
@@ -1,11 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace BMS.Data.Models
+﻿namespace BMS.Data.Models
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using System.ComponentModel.DataAnnotations;
+    using BMS.GlobalData.ErrorMessages;
+
     public class Suitcase
     {
+
+        public Suitcase()
+        {
+            this.SuitcaseId = Guid.NewGuid().ToString();
+        }
+
+        [Key]
+        public string SuitcaseId { get; set; }
+
+        [Range(1,32, ErrorMessage = InvalidErrorMessages.PAXSuitcaseWeightIsInvalid)]
+        public int Weight { get; set; }
+
+        
+        public int PaxId { get; set; }
+
+        public Passenger Passenger { get; set; }
     }
 }
---
+++ WebApplication1/GlobalData/Validation/PAXInputDataValidation.cs
@@ -0,0 +1,13 @@
+﻿namespace BMS.GlobalData.Validation
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    public class PAXInputDataValidation
+    {
+
+        public const string PAXGenderValidation = @"^[MFiC]$"; 
+    }
+}
