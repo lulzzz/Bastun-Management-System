86a5b4e1ebf1acf572ee092c5731be45ac42ba7d
Mitko <34096775+Shemetnia@users.noreply.github.com>
2020-04-04 12:49:39 +0300

    clean home controller

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -14,12 +14,11 @@ namespace WebApplication1.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-        private readonly IFlightService flightService;
 
-        public HomeController(ILogger<HomeController> logger, IFlightService flightService)
+
+        public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
-            this.flightService = flightService;
         }
 
         [HttpGet]
