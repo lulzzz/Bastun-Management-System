ddf85df785b2852ff00495cebda95d5ad40f607d
Mitko <34096775+Shemetnia@users.noreply.github.com>
2020-04-06 15:12:34 +0300

    change departure, arrival movement views

--- WebApplication1/Models/MovementsInputModels/MovementInputModel.cs
+++ WebApplication1/Models/MovementsInputModels/MovementInputModel.cs
@@ -1,11 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace BMS.Models.MovementsInputModels
+﻿namespace BMS.Models.MovementsInputModels
 {
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.Linq;
+    using System.Threading.Tasks;
+
     public class MovementInputModel
     {
+        [Required]
+        public string ArrivalMovement { get; set; }
+
+        [Required]
+        public string DepartureMovement { get; set; }
     }
 }
