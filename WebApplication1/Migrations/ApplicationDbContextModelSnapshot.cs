105bd54c3eb7d91c668a99487edae672f6b03465
Mitko <34096775+Shemetnia@users.noreply.github.com>
2020-04-09 00:51:26 +0300

    fixed database ;)
    What did we learn today? To always plan your projects in  a concise manner and to make proper architecture!!!

--- WebApplication1/Migrations/ApplicationDbContextModelSnapshot.cs
+++ WebApplication1/Migrations/ApplicationDbContextModelSnapshot.cs
@@ -3,17 +3,15 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
-using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WebApplication1.Data;
 
-namespace WebApplication1.Data.Migrations
+namespace BMS.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20200408213619__test_3")]
-    partial class _test_3
+    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
     {
-        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        protected override void BuildModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
