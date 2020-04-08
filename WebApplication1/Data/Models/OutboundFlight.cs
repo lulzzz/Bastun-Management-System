73be978255b648199ae7a5cfaa7a7c0227d9691c
Mitko <34096775+Shemetnia@users.noreply.github.com>
2020-04-08 11:22:07 +0300

    create inbound/outbound flight models

---
+++ WebApplication1/Data/Models/OutboundFlight.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BMS.Data.Models
+{
+    public class OutboundFlight
+    {
+    }
+}
