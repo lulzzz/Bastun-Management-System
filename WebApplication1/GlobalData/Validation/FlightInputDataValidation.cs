686629121aa91caaf54eee4a87b347bf6d0b26bb
Mitko <34096775+Shemetnia@users.noreply.github.com>
2020-04-08 11:31:41 +0300

    change validation for input flight data validation

--- WebApplication1/GlobalData/Validation/FlightInputDataValidation.cs
+++ WebApplication1/GlobalData/Validation/FlightInputDataValidation.cs
@@ -6,13 +6,13 @@
     using System.Threading.Tasks;
     public static class FlightInputDataValidation
     {
-        public const string FlightNumberValidation = @"^[A-Z]{1,4}\-[0-9]{1,4}$";
+        public const string FlightNumberValidation = @"(?<inbound>[A-Z]{1,5}[0-9]{1,5})\/(?<outbound>[A-Z]{1,5}[0-9]{1,5})";
 
-        public const string AircraftRegistrationValidation = @"^[A-Z]{1,2}\-[A-Z0-9]{1,10}$";
+        public const string AircraftRegistrationValidation = @"^[A-Z]{1,2}[A-Z0-9]{1,10}$";
 
         //TODO: Make validation for each manufacturer 
         public const string AircraftTypeValidation = @"[B737]+|[A320]+";
 
-        public const string AircraftVersionValidation = @"[Y][1][8][0-9]";
+        public const string AircraftVersionValidation = @"(?<version>[Y][0-9]{1,3})";
     }
 }
