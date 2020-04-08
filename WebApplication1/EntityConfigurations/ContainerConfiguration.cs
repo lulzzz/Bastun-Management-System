0f3cfbfe415311fc97de3bc76e97fe2915f60ccf
Mitko <34096775+Shemetnia@users.noreply.github.com>
2020-04-09 00:03:00 +0300

    add containerInfo entity configuration

---
+++ WebApplication1/EntityConfigurations/ContainerConfiguration.cs
@@ -0,0 +1,22 @@
+﻿namespace BMS.EntityConfigurations
+{
+    using System;
+    using System.Collections.Generic;
+    using BMS.Models;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using BMS.Data.Models;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+    public class ContainerConfiguration : IEntityTypeConfiguration<Container>
+    {
+        public void Configure(EntityTypeBuilder<Container> builder)
+        {
+            builder.HasOne(x => x.ContainerInfo)
+                .WithOne(x => x.Container)
+                .HasForeignKey<ContainerInfo>(x => x.ContainerInfoId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
