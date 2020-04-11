e5d229c2e6188dc347fdab55920b2daa5ccdb210
Mitko <34096775+Shemetnia@users.noreply.github.com>
2020-04-11 17:01:19 +0300

    add regex for ldm flight validation

--- WebApplication1/Services/Utility/UtilityConstants/FlightInfoConstants.cs
+++ WebApplication1/Services/Utility/UtilityConstants/FlightInfoConstants.cs
@@ -9,6 +9,6 @@
     {
         public const string IsFlightInfoValid = @"(?<flt>[A-Z]{1,4}[0-9]{1,5})\/(?<date>[0-9]{1,2})\.(?<reg>[A-Z0-9]{5,10})\.(?<origin>[A-Z]{3})";
 
-        public const string IsLDMFlightInfoValid = @""; 
+        public const string IsLDMFlightInfoValid = @"(?<flt>[A-Z]{1,3}[0-9]{1,5})\/(?<date>[0-9]{1,2})\.(?<reg>[A-Z0-9]{1,5})\.(?<config>[A-Z]{1}[0-9]{3})\.(?<crewConfig>[0-9]{1,5}\/[0-9]{1,16})"; 
     }
 }
