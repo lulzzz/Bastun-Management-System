7930a782d18e889fa5fdfcff6b2e066ed5ffa7af
Mitko <34096775+Shemetnia@users.noreply.github.com>
2020-04-11 22:01:57 +0300

    add functionality for show/hide register form

---
+++ WebApplication1/wwwroot/js/ShowHideRegisterForm.js
@@ -0,0 +1,17 @@
+﻿function showHideRegisterForm() {
+    let displayButton = document.getElementById('registerButton');
+    let form = document.getElementById('registerForm');
+
+    if (displayButton.textContent === "Display") {
+        displayButton.textContent = "Hide";
+    } else {
+        displayButton.textContent = "Display";
+    }
+
+    if (form.style.display === "none") {
+        form.style.display = "block";
+    } else {
+        form.style.display = "none";
+    }
+
+}
