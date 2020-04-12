053bc8d7021faa6e16aa7224be5d0cea3a0b758b
Mitko <34096775+Shemetnia@users.noreply.github.com>
2020-04-12 11:25:27 +0300

    create view models for inbound/outbound flights

---
+++ WebApplication1/Models/ViewModels/Flights/InboundFlightsViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace BMS.Models.ViewModels.Flights
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    public class InboundFlightsViewModel
+    {
+        public InboundFlightsViewModel()
+        {
+
+        }
+
+        public List<i> MyProperty { get; set; }
+    }
+}
