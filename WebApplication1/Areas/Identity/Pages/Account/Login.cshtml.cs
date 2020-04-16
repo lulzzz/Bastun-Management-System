b49c0943d1ec7a6661ec8e0631b9ac3c152d77be
Mitko <predator131@mail.bg>
2020-04-16 14:29:31 +0300

    Finish work on FINAL login user view

--- WebApplication1/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ WebApplication1/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -42,13 +42,16 @@ namespace BMS.Areas.Identity.Pages.Account
 
         public class InputModel
         {
-            [Required]
-            [EmailAddress]
-            public string Email { get; set; }
+            [Required(ErrorMessage = "Username is required")]
+            public string UserName { get; set; }
 
             [Required]
             [DataType(DataType.Password)]
             public string Password { get; set; }
+
+            [Required(ErrorMessage = "Password confirmation is required")]
+            [DataType(DataType.Password)]
+            public string ConfirmPassword { get; set; }
 
             [Display(Name = "Remember me?")]
             public bool RememberMe { get; set; }
@@ -79,7 +82,7 @@ namespace BMS.Areas.Identity.Pages.Account
             {
                 // This doesn't count login failures towards account lockout
                 // To enable password failures to trigger account lockout, set lockoutOnFailure: true
-                var result = await _signInManager.PasswordSignInAsync(Input.Email, Input.Password, Input.RememberMe, lockoutOnFailure: false);
+                var result = await _signInManager.PasswordSignInAsync(Input.UserName, Input.Password, Input.RememberMe, lockoutOnFailure: false);
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User logged in.");
